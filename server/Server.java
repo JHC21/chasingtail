8c665ba92600fb446b3c60abdb51265b1ec441dd
JHC21 <57344372+JHC21@users.noreply.github.com>
2019-11-04 17:53:52 +0900

    Update Server.java

--- server/Server.java
+++ server/Server.java
@@ -1,10 +1,36 @@
-import java.net.serverSocket;
+package Server;
 
-public class Server{
-  
-  public static void main(String args[]){
-    
-    
-  }
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class Server {
+
+    public static void main(String[] args) throws SocketException {
+
+        int port;
+        Lobby room[];
+
+        if(args.length != 2){
+            return;
+        } else {
+            port = Integer.valueOf(args[1]);
+            room = new Lobby[3];
+        }
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            Socket listenSocket = serverSocket.accept();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
+
 //https://lktprogrammer.tistory.com/64
