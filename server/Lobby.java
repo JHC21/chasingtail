ef8e6cca155709b8615236a5ca5227bcb691dae7
JHC21 <57344372+JHC21@users.noreply.github.com>
2019-11-26 17:44:08 +0900

    Update Lobby.java

--- server/Lobby.java
+++ server/Lobby.java
@@ -1,35 +1,19 @@
-public class Lobby {
-    private char num;
-    User masterClient;
-    User guest[];
-    boolean isStart;
+package Server;
 
-    Lobby(User masterClient){
-        num = 0;
-        this.masterClient = masterClient;
-        guest = new User[3];
-        isStart = false;
-    }
+import java.util.List;
 
-    public int addClient(User newUser){ // return client number
-        if(num < 4){
-            guest[num] = newUser;
-            num++;
-            return num;
-        } else {
-            return -1;
+public class Lobby extends Thread{
+    private Room r[];
+    List<User> list;
+
+
+
+
+    Lobby(){
+        r = new Room[3];
+        for(int i=0; i<3;i++){
+            r[i] = new Room();
         }
     }
-
-    public boolean isReady(){
-        if(num == 0){
-            return false;
-        } else {
-          for(int i=0; i<num;i++){
-            if(!guest[i].getisReady())
-                return false;
-          }
-          return true;
-        }
-    }
+    
 }
